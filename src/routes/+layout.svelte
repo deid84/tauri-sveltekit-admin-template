0d2e4786fa5cf27f07ecb7b473073f4c887129df
deid84 <d.ferrero@me.com>
2023-07-05 22:58:55 +0200

    started implementing layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
-  import "../app.css";
+	import '../app.css';
+	import Sidebar from '$lib/components/layout/sidebar.svelte';
 </script>
 
-<slot />
+<!-- scroll to top button -->
+<div class="fixed bottom-6 z-50 right-6">
+	<button type="button" class="btn btn-outline-primary animate-pulse rounded-full bg-[#fafafa] p-2 dark:bg-[#060818] dark:hover:bg-primary">
+		<svg width="24" height="24" class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M12 20.75C12.4142 20.75 12.75 20.4142 12.75 20L12.75 10.75L11.25 10.75L11.25 20C11.25 20.4142 11.5858 20.75 12 20.75Z" fill="currentColor"></path>
+			<path d="M6.00002 10.75C5.69667 10.75 5.4232 10.5673 5.30711 10.287C5.19103 10.0068 5.25519 9.68417 5.46969 9.46967L11.4697 3.46967C11.6103 3.32902 11.8011 3.25 12 3.25C12.1989 3.25 12.3897 3.32902 12.5304 3.46967L18.5304 9.46967C18.7449 9.68417 18.809 10.0068 18.6929 10.287C18.5768 10.5673 18.3034 10.75 18 10.75L6.00002 10.75Z" fill="currentColor"></path>
+		</svg>
+	</button>
+</div>
+
+<!-- main container -->
+<div class="main-container min-h-screen text-black dark:text-white-dark navbar-sticky">
+	<!-- sidebar section -->
+	<Sidebar />
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <a href="/about">About my site</a>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.800);
-  }
-</style>

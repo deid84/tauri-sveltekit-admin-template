a77147e79c96692b696b9d4c48ab3af4ac1b06e8
Davide Ferrero <d.ferrero@me.com>
2023-07-05 17:11:32 +0200

    succesfully imported tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <a href="/about">About my site</a>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.800);
+  }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+  darkMode: 'class',
+}
+

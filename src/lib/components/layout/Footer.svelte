030269cce10466e816430356ad07941c1678e0f2
Davide Ferrero <d.ferrero@me.com>
2023-07-11 14:14:47 +0200

    removed footer bg

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import node_logo from '$lib/images/node.png';
+	import node_logo from '$lib/images/tauri.png';
 	import svelte_logo from '$lib/images/svelte.png';
 
 	const date = new Date().getFullYear();
@@ -7,7 +7,7 @@
 
 <!-- Footer starts -->
 <footer
-	class="fixed bottom-0 right-0 h-16 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700"
+	class="fixed bottom-0 right-14 h-16"
 >
 	<!-- <div class="flex-none"> -->
 	<div
@@ -18,12 +18,12 @@
 			<span>© {date} Copyright:</span>
 			<a
 				class="text-gray-700 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
-				href="http://www.capetti.it/"
-				target="blank">Capetti Elettronica S.r.l.</a
+				href="#"
+				target="blank">Your Brand</a
 			>
 		</div>
 		<div class="flex mr-2">
-			<img src={node_logo} alt="nodejs" title="Node.js" class=" my-2 mr-2" />
+			<img src={node_logo} alt="tauri" title="Tauri" class=" my-2 mr-2" />
 			<img src={svelte_logo} alt="sveltejs" title="Svelte" class=" my-2" />
 		</div>
 	</div>
